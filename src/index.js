8b41539c05678652c09e7476862d0dc2b663be6a
mstein <mstein@cardknox.com>
2020-08-07 14:50:13 -0400

    added process function, added track device in progress

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-import { SignatureReader } from "./Pax";
+import { SignatureReader as PaxSignatureReader, CardReader as PaxCardReader } from "./Pax";
 import { Device, DeviceIpReader, IpDeviceCommunicator } from './Device'
+
+/**
+ * 
+ * @param {TransactionRequest} request 
+ */
+export async function process(request) {
+    try {
+        const ipReader = new DeviceIpReader(request.settings);
+        const device = new Device({
+            cardReader: new PaxCardReader(
+                new IpDeviceCommunicator(ipReader.getIP(), request.settings.deviceIpPort, 'http')
+            )
+        });
+        updateInProgress(request.settings.deviceIpAddress, device);
+        return await device.process(request);
+    } catch (error) {
+        return {
+            xResult: "E",
+            xStatus: "Error",
+            xError: error.toString()
+        }
+    } finally {
+        inProgress[request.settings.deviceIpAddress] = undefined;
+    }
+}
 
 /**
  * 
@@ -8,12 +33,43 @@ import { Device, DeviceIpReader, IpDeviceCommunicator } from './Device'
  * @description returns the image *without* the data header
  */
 export async function getSignature(request) {
-    const ipReader = new DeviceIpReader(request);
-    const device = new Device({
-        signatureReader: new SignatureReader(
-            new IpDeviceCommunicator(ipReader.getIP(), request.deviceIpPort, 'http')
-        )
-    });
-    return await device.getSignature();
+    try {
+        const ipReader = new DeviceIpReader(request);
+        const device = new Device({
+            signatureReader: new PaxSignatureReader(
+                new IpDeviceCommunicator(ipReader.getIP(), request.deviceIpPort, 'http')
+            )
+        });
+        updateInProgress(request.deviceIpAddress, device)
+        return await device.getSignature();
+    } catch (error) {
+        return {
+            xResult: "E",
+            xStatus: "Error",
+            xError: error.toString()
+        }
+    } finally {
+        inProgress[request.deviceIpAddress] = undefined;
+    }
 }
 
+export function cancel(request) {
+    try {
+        const ipReader = new DeviceIpReader(request);
+        const device = new Device();
+        device.cancel(new IpDeviceCommunicator(ipReader.getIP(), request.deviceIpPort, 'http'));
+    } catch (error) {
+        console.error(error)
+        throw error;
+    }
+}
+
+const inProgress = {};
+
+function updateInProgress(ip, device) {
+    if (inProgress[ip]) {
+        throw 'Transaction in progress';
+    } else {
+        inProgress[ip] = device;
+    }
+}
