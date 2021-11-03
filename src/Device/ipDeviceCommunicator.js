ae228d02e5d8ef4c64bbfd651603ddb928e0deab
MStein <47797106+mstein-ck@users.noreply.github.com>
2021-11-03 14:52:07 -0400

    Add ip to dnsimple no cors (#48)
    * add ip to dnsimple no-cors
    
    * Update .gitignore
    
    * Update ipDeviceCommunicator.js
    
    * Update src/Device/ipDeviceCommunicator.js
    
    Co-authored-by: ymwymw <31081824+ymwymw@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/Device/ipDeviceCommunicator.js
+++ src/Device/ipDeviceCommunicator.js
@@ -1,6 +1,8 @@
 import { readAll, last } from "../core/core";
 import isIp from "is-ip";
 
+const LOCAL_DEVICE_TOOL_URL = '/';      //TODO: Use correct endpoint when app is released
+
 export default class IpDeviceCommunicator {
     constructor(ip, port, protocol) {
         this._ip = ip;
@@ -39,7 +41,26 @@ export default class IpDeviceCommunicator {
             return chunk;
         } catch (error) {
             console.error(error);
+            // await this.tryRegisterIp(); //TODO: Enable once LOCAL_DEVICE_TOOL_URL endpoint is available
             throw error;
         }
+    }
+
+    tryRegisterIp = async () => {
+        try {
+            if (!this.isHttps)
+                return;
+            await fetch(LOCAL_DEVICE_TOOL_URL, {
+                body: `ip=${this._ip}`,
+                method: 'POST',
+                mode: 'no-cors',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+        } catch (error) {
+            console.error(error);
+        }
+        
     }
 }
