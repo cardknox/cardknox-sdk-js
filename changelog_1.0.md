3207ecef41a936467b34aaff26229f790ad426f2
mstein <mstein@cardknox.com>
2022-01-18 15:06:36 -0500

    publish-action

---
+++ changelog_1.0.md
@@ -0,0 +1,25 @@
+
+<head>
+    <title>JS SDK v1.0 - Change Log</title>
+</head>
+
+### JS SDK v1.0 - Change Log
+
+#### [1.0.2111.1801-beta] - 2021-11-18
+- Convert IP address to domain using 'mylocaldevice.com' when using _https_
+
+#### [1.0.2103.1801-beta] - 2021-03-18
+- Added xCardType to the response
+
+#### [1.0.2011.0401-beta] - 2020-11-04
+- Fix for credit
+
+#### [1.0.2009.1101-alpha] - 2020-09-11
+- Fixed protocol bug
+- Fixed amount format
+
+#### [1.0.2008.2401-beta] - 2020-08-24
+- Initial release
+- Contains basic features:
+  - Sale
+  - Signature
---
+++ functions.js
@@ -0,0 +1,170 @@
+
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const { Transform } = require('stream');
+
+/**
+ * 
+ * @typedef Version
+ * @property {string} major
+ * @property {string} minor
+ * @property {string} patch
+ * @property {string} deployment - Is empty when deployment is 'stable'
+ * @property {() => string} getFullVersion
+ */
+
+/**
+ * 
+ * Relies on 'package.json' for the version number (including the date and id portion)
+ * @param {string} deployment
+ * @returns {Version}
+ */
+function getVersion(deployment) {
+  const versionNumber = require('./package.json').version;
+  const parts = versionNumber.split('.');
+  const patch = `${parts[2]}.${parts[3]}`;
+  return {
+    major: parts[0],
+    minor: parts[1],
+    patch,
+    deployment,
+    getFullVersion: () => `${parts[0]}.${parts[1]}.${patch}${deployment ? '-' + deployment : ''}`
+  };
+}
+
+/**
+ * 
+ * Uses webpack to build the project
+ * Will reject on errors or warnings; error result will contain the errors or warnings
+ * @param webpackConfig
+ * @returns {Promise<Object>} Promise representing the result of the build
+ */
+function buildProjectAsync(webpackConfig) {
+  return new Promise((resolve, reject) => {
+    webpack(webpackConfig, (err, stats) => {
+      if (err)
+        reject(err);
+      const info = stats.toString('summary');
+      if (stats.hasErrors() || stats.hasWarnings())
+        reject(info);
+      resolve(info);
+    });
+  });
+}
+
+/**
+ * Transforms a Markdown changelog to html using the 'showdown' module
+ * @param {string} source - File path of source
+ * @param {string} destination - File path of destination
+ */
+function generateHtmlChangelog(source, destination) {
+  const showdown = new (require('showdown').Converter)({ completeHTMLDocument: true, noHeaderId: true });
+  showdown.setFlavor('github');
+  fs.createReadStream(source)
+    .pipe(new Transform({
+      transform: (chunk, encoding, done) => {
+        const result = showdown.makeHtml(chunk.toString());
+        done(null, result);
+      }
+    }))
+    .pipe(fs.createWriteStream(destination));
+}
+
+/**
+ * 
+ * @typedef {import('aws-sdk').S3.PutObjectRequest} PutObjectRequest
+ */
+
+/**
+ * 
+ * @typedef {import('aws-sdk').S3} S3
+ */
+
+/**
+ * 
+ * @typedef UploadOptions
+ * @property {string} localPath
+ * @property {string} key
+ * @property {string} bucketName
+ */
+
+/**
+ * 
+ * @param {S3} s3
+ * @param {UploadOptions} options
+ * @returns {Promise<PutObjectRequest>} Promise resulting with the response
+ * @throws AWS.AWSError
+ */
+async function uploadLocalFileAsync(s3, options) {
+  try {
+    const contents = await readFileAsync(options.localPath);
+    return await s3.putObject({
+      Bucket: options.bucketName,
+      Key: options.key,
+      Body: contents,
+      ServerSideEncryption: 'AES256',
+      ContentType: getContentType(options.localPath)
+    }).promise();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+/**
+ * 
+ * @param {string} filePath 
+ * @returns {Promise<boolean>} Promise resulting with the file-exist check
+ */
+function fileExistsAsync(filePath) {
+  return new Promise((resolve) => {
+    fs.stat(filePath, err => {
+      if (err)
+        resolve(false);
+      resolve(true);
+    });
+  })
+}
+
+/**
+ * 
+ * Using 'UTF-8' encoding
+ * @param {string} filePath 
+ * @returns {Promise<string>} Promise resulting with the contents of the file
+ */
+function readFileAsync(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err)
+        reject(err);
+      else
+        resolve(data);
+    });
+  });
+}
+
+/**
+ * 
+ * Returns MIME type for js and html files
+ * @param {string} fileName 
+ * @returns {string} MIME type
+ */
+function getContentType(fileName) {
+  switch (path.extname(fileName)) {
+    case '.html':
+      return 'text/html';
+    case '.js':
+      return 'text/javascript'
+    default:
+      return 'text/plain';
+  }
+}
+
+module.exports = {
+  getVersion,
+  buildProjectAsync,
+  generateHtmlChangelog,
+  uploadLocalFileAsync,
+  fileExistsAsync
+}
